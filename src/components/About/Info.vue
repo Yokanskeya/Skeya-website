54753c001717b15eb07b6afee63d8195e04d1e12
Skeya <75810467+Yokanskeya@users.noreply.github.com>
2023-08-01 20:00:37 +0700

    Skeya

--- src/components/About/Info.vue
+++ src/components/About/Info.vue
@@ -3,6 +3,9 @@
         I'm a self-taught developer from Jakarta/Indonesia. I'm currently as graduate college student, 
         interested in every aspect of computer science, electronics, science and body building. To be honest, 
         I'm a bit of a nerd. I love to learn and I always try to improve myself. I enjoy reading and watching about 
-        any topic but my favorites are sci-fi, psychology, history and self-improvement.
+        any topic but my favorites are sci-fi, psychology, history and self-improvement. Currently i'm doing develop my 
+        <span class="font-bold">Discord bot</span>
+        at
+        <Link href="https://mitybot.xyz/" label="Mity" class="mx-1" />.
     </Card>
 </template>
