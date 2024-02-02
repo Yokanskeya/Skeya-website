d4e1c7fa38621c2782246ea0296eda1e8c0ed94b
Skeya <75810467+Yokanskeya@users.noreply.github.com>
2024-02-02 19:29:58 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
     const show_ = ref(false)
 
     useHead({
-        titleTemplate: (title?: string) => (title ? `${title} | Skeya` : 'Skeya'),
+        titleTemplate: (title?: string) => (title ? `${title} | K1zen` : 'K1zen'),
         link: [{ rel: 'icon', type: 'image/png', sizes: 'any', href: '/favicon.png' }]
     })
 
